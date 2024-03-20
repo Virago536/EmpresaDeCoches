90839bb82152e9f7d66943b8f44e493641b53d02
Raúl Huertas <146868448+raulhuertass@users.noreply.github.com>
2024-03-20 08:58:21 +0100

    Furgoneta

--- examenDual/src/Furgoneta.java
+++ examenDual/src/Furgoneta.java
@@ -1,5 +1,30 @@
 package com.politecnico.coches;
 
-public class Furgoneta {
+public class Furgoneta extends Vehiculo{
 
+    float cargaMaxima;
+    int plazaAsientos;
+
+    public Furgon(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor, float cargaMaxima, int plazaAsientos) {
+        super(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor);
+        this.cargaMaxima = cargaMaxima;
+        this.plazaAsientos = plazaAsientos;
+
+    }
+
+    public float getCargaMaxima() {
+        return cargaMaxima;
+    }
+
+    public void setCargaMaxima(float cargaMaxima) {
+        this.cargaMaxima = cargaMaxima;
+    }
+
+    public int getPlazaAsientos() {
+        return plazaAsientos;
+    }
+
+    public void setPlazaAsientos(int plazaAsientos) {
+        this.plazaAsientos = plazaAsientos;
+    }
 }

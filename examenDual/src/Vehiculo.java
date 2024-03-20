654dd81cc8be7afaf76b8b855c7771b2b1abd9cc
Raúl Huertas <146868448+raulhuertass@users.noreply.github.com>
2024-03-20 08:56:49 +0100

    vehiculo

--- examenDual/src/Vehiculo.java
+++ examenDual/src/Vehiculo.java
@@ -1,5 +1,66 @@
 package com.politecnico.coches;
 
-public class Vehiculo {
+ String matricula;
+    String modelo;
+    String marca;
+    float kmRecorridos;
+    float precioDia;
+    String tipoMotor;
 
+    public Vehiculo(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.kmRecorridos = kmRecorridos;
+        this.precioDia = precioDia;
+        this.tipoMotor = tipoMotor;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getKmRecorridos() {
+        return kmRecorridos;
+    }
+
+    public void setKmRecorridos(float kmRecorridos) {
+        this.kmRecorridos = kmRecorridos;
+    }
+
+    public float getPrecioDia() {
+        return precioDia;
+    }
+
+    public void setPrecioDia(float precioDia) {
+        this.precioDia = precioDia;
+    }
+
+    public String getTipoMotor() {
+        return tipoMotor;
+    }
+
+    public void setTipoMotor(String tipoMotor) {
+        this.tipoMotor = tipoMotor;
+    }
 }

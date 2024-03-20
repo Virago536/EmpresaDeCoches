efa17d4cf5e94cd2dbade4ca6d51d357978d0f14
Virago536 <125345810+Virago536@users.noreply.github.com>
2024-03-20 09:11:10 +0100

    Update Empresa.java

--- examenDual/src/Empresa.java
+++ examenDual/src/Empresa.java
@@ -1,5 +1,50 @@
 package com.politecnico.coches;
 
-public class Empresa {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Empresa implements GestorCliente{
+	String nombre;
+	
+	public Map <String,Cliente> mapaClientes = new HashMap<String, Cliente>();
+	public ArrayList<Coche> listaCoches;
+	public ArrayList<Furgoneta> listaFurgonetas;
+	public ArrayList<Camion> listaCamiones;
+	public ArrayList<Vehiculo> listaVehiculos;
+	
+	public Empresa(String nombre, Map<String, Cliente> mapaClientes, ArrayList<Coche> listaCoches, ArrayList<Furgoneta> listaFurgonetas, ArrayList<Camion> listaCamiones) {
+		this.nombre = nombre;
+		mapaClientes = new HashMap<>();
+		listaCoches = new ArrayList<>();
+		listaFurgonetas = new ArrayList<>();
+		listaCamiones = new ArrayList<>();
+	}
+	
+	
+	//Métodos de la clase cliente
+	public boolean addCliente(Cliente c) {
+		if(c!=null && !mapaClientes.containsValue(c)) {
+			mapaClientes.put(c.getDni(), c);
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean removeCliente(Cliente c) {
+		if(c!=null && mapaClientes.containsValue(c)) {
+			mapaClientes.remove(c.getDni(), c);
+			return true;
+		}
+		else return false;
+	}
+	
+	public Cliente buscarCliente(String dni) {
+		if(dni!=null) {
+			return mapaClientes.get(dni);
+		}
+		else return null;
+	}
+	
+	
 }

9a3d40c278e25ad523187cc11a360581beb83a1e
Raúl Huertas <146868448+raulhuertass@users.noreply.github.com>
2024-03-20 08:58:57 +0100

    camion

--- examenDual/src/Camion.java
+++ examenDual/src/Camion.java
@@ -1,5 +1,29 @@
 package com.politecnico.coches;
 
-public class Camion {
+public class Camion extends Vehiculo{
 
+    float cargaMaxima;
+    float longitud;
+
+    public Camion(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor, float cargaMaxima, float longitud) {
+        super(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor);
+        this.cargaMaxima = cargaMaxima;
+        this.longitud = longitud;
+    }
+
+    public float getCargaMaxima() {
+        return cargaMaxima;
+    }
+
+    public void setCargaMaxima(float cargaMaxima) {
+        this.cargaMaxima = cargaMaxima;
+    }
+
+    public float getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(float longitud) {
+        this.longitud = longitud;
+    }
 }

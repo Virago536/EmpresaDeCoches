00eabcfe1310c4b8141f92de10f7514fddd879a7
Virago536 <125345810+Virago536@users.noreply.github.com>
2024-03-22 08:52:42 +0100

    Update GestorCliente.java

--- examenDual/src/GestorCliente.java
+++ examenDual/src/GestorCliente.java
@@ -1,5 +1,11 @@
 package com.politecnico.coches;
 
 public interface GestorCliente {
-
+	
+	public boolean addCliente(Cliente c);
+	
+	public boolean removeCliente(Cliente c);
+	
+	public Cliente buscarCliente(String dni);
 }
+

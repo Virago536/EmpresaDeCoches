9b67694377862b01ea0f8d706e13e5486d3d8a1f
Virago536 <125345810+Virago536@users.noreply.github.com>
2024-03-22 08:50:19 +0100

    Update Empresa.java

--- examenDual/src/Empresa.java
+++ examenDual/src/Empresa.java
@@ -4,103 +4,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.politecnicomalaga.coches.Coche;
-
 public class Empresa implements GestorCliente{
-	String nombre;
+	private String nombre;
 	
-	public Map <String,Cliente> mapaClientes = new HashMap<String, Cliente>();
+	public ArrayList<Vehiculo> listaVehiculos;
 	public ArrayList<Coche> listaCoches;
 	public ArrayList<Furgoneta> listaFurgonetas;
 	public ArrayList<Camion> listaCamiones;
-	public ArrayList<Vehiculo> listaVehiculos;
+	public Map <String, Cliente> mapaClientes = new HashMap<String, Cliente>();
+ 
 	
-	public Empresa(String nombre, Map<String, Cliente> mapaClientes, ArrayList<Coche> listaCoches, ArrayList<Furgoneta> listaFurgonetas, ArrayList<Camion> listaCamiones) {
+	public Empresa(String nombre) {
 		this.nombre = nombre;
-		mapaClientes = new HashMap<>();
 		listaCoches = new ArrayList<>();
 		listaFurgonetas = new ArrayList<>();
 		listaCamiones = new ArrayList<>();
+		mapaClientes = new HashMap<>();
 	}
 	
-	
-	//Métodos de la clase cliente
-	public boolean addCliente(Cliente c) {
-		if(c!=null && !mapaClientes.containsValue(c)) {
-			mapaClientes.put(c.getDni(), c);
-			return true;
+	public boolean addFurgoneta(String matricula, String modelo, String marca, String motor, int kms_recorridos, float precio_dia, String conductor, int dias_alquilado, int carga, int asientos) {
+		Furgoneta furgoNueva = new Furgoneta(matricula, modelo, marca, motor, kms_recorridos, precio_dia, conductor, dias_alquilado, carga, asientos);
+		if(!this.estaFurgoneta(matricula)) {
+		listaFurgonetas.add(furgoNueva);
+		return true;
 		}
 		else return false;
-	}
-	
-	public boolean removeCliente(Cliente c) {
-		if(c!=null && mapaClientes.containsValue(c)) {
-			mapaClientes.remove(c.getDni(), c);
-			return true;
-		}
-		else return false;
-	}
-	
-	public Cliente buscarCliente(String dni) {
-		if(dni!=null) {
-			return mapaClientes.get(dni);
-		}
-		else return null;
-	}
-	
-	public boolean addCoche(String matricula, String modelo, String marca, float kmRecorridos, float precioDia,String tipoMotor, int plazasMaximas, int numPuertas, float volumenMaletero, String tipo) {
-		Coche cocheNuevo = new Coche(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor, plazasMaximas, numPuertas, volumenMaletero, tipo);
-		if(!this.estaCoche(matricula)) {
-			listaVehiculos.add(cocheNuevo);
-			return true;
-		}else return false;
-	}
-	
-	public boolean estaCoche(String matricula) {
-		return (buscaCoche(matricula)!=null);
-	}
-	
-	public Coche buscaCoche(String matricula) {
-		for(Coche cualquierCoche: listaCoches) {
-			if(cualquierCoche.getMatricula().equals(matricula)) {
-				return cualquierCoche;
-			}
-		}
-		return null;
-	}
-	
-	public boolean addCamion(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor, float cargaMaxima, float longitud) {
-		Camion camionNuevo = new Camion(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor, cargaMaxima, longitud);
-		if(!this.estaCamion(matricula)) {
-			listaVehiculos.add(camionNuevo);
-			return true;
-		}else return false;
-	}
-	
-	public boolean estaCamion(String matricula) {
-		return (buscaCamion(matricula)!=null);
-	}
-	
-	public Camion buscaCamion(String matricula) {
-		for(Camion cualquierCamion: listaCamiones) {
-			if(cualquierCamion.getMatricula().equals(matricula)) {
-				return cualquierCamion;
-			}
-		}
-		return null;
-	}
-	
-	public boolean addFurgoneta(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor, float cargaMaxima, int plazaAsientos) {
-		Furgoneta furgoNueva = new Furgoneta(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor, cargaMaxima, plazaAsientos);
-		if(!this.estaCamion(matricula)) {
-			listaFurgonetas.add(furgoNueva);
-			return true;
-		}else return false;
-	}
+	} 
 	
 	public boolean estaFurgoneta(String matricula) {
-		return (buscaCamion(matricula)!=null);
+		return (buscaFurgoneta(matricula)!=null);
 	}
+	
 	
 	public Furgoneta buscaFurgoneta(String matricula) {
 		for(Furgoneta cualquierFurgo: listaFurgonetas) {
@@ -111,6 +45,137 @@ public class Empresa implements GestorCliente{
 		return null;
 	}
 	
+	public boolean removeFurgoneta(String matricula) {
+		for(Furgoneta cualquierFurgo: listaFurgonetas) {
+			if(cualquierFurgo.getMatricula().equals(matricula)) {
+				listaFurgonetas.remove(cualquierFurgo);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Furgoneta[] listarLasFurgonetas() {
+		Furgoneta[] nuevaListaFurgos = new Furgoneta[listaFurgonetas.size()];
+		
+		for(int i = 0; i<nuevaListaFurgos.length; i++) {
+			nuevaListaFurgos[i] = listaFurgonetas.get(i);
+		}
+		return nuevaListaFurgos;
+		
+	}
+	
+	public boolean addCoche(String matricula, String modelo, String marca, String motor, int kms_recorridos, float precio_dia, String conductor, int dias_alquilado, int plazas, int puertas, int v_maletero, String tipo) {
+		Coche cocheNuevo = new Coche(matricula, modelo, marca, motor, kms_recorridos, precio_dia, conductor, dias_alquilado, plazas, puertas, v_maletero, tipo);
+		if(!this.estaCoche(matricula)) {
+		listaCoches.add(cocheNuevo);
+		return true;
+		}
+		else return false;
+	} 
+	
+	public boolean estaCoche(String matricula) {
+		return (buscaCoche(matricula)!=null);
+	}
 	
 	
+	public Coche buscaCoche(String matricula) {
+		for(Coche cualquierCoche: listaCoches) {
+			if(cualquierCoche.getMatricula().equals(matricula)) {
+				return cualquierCoche;
+			}
+		}
+		return null;
+	}
+	
+	public boolean removeCoche(String matricula) {
+		for(Coche cualquierCoche: listaCoches) {
+			if(cualquierCoche.getMatricula().equals(matricula)) {
+				listaCoches.remove(cualquierCoche);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Coche[] listarLosCoches() {
+		Coche[] nuevaListaCoches = new Coche[listaCoches.size()];
+		
+		for(int i = 0; i<nuevaListaCoches.length; i++) {
+			nuevaListaCoches[i] = listaCoches.get(i);
+		}
+		return nuevaListaCoches;
+		
+	}
+	
+	public boolean addCamion(String matricula, String modelo, String marca, String motor, int kms_recorridos, float precio_dia, String conductor, int dias_alquilado, int carga, int longitud) {
+		Camion camionNuevo = new Camion(matricula, modelo, marca, motor, kms_recorridos, precio_dia, conductor, dias_alquilado, carga, longitud);
+		if(!this.estaCamion(matricula)) {
+		listaCamiones.add(camionNuevo);
+		return true;
+		}
+		else return false;
+	} 
+	
+	public boolean estaCamion(String matricula) {
+		return (buscaCoche(matricula)!=null);
+	}
+	
+	
+	public Camion buscaCamion(String matricula) {
+		for(Camion cualquierCamion: listaCamiones) {
+			if(cualquierCamion.getMatricula().equals(matricula)) {
+				return cualquierCamion;
+			}
+		}
+		return null;
+	}
+	
+	public boolean removeCamion(String matricula) {
+		for(Coche cualquierCoche: listaCoches) {
+			if(cualquierCoche.getMatricula().equals(matricula)) {
+				listaCoches.remove(cualquierCoche);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Camion[] listarLosCamiones() {
+		Camion[] nuevaListaCamiones = new Camion[listaCamiones.size()];
+		
+		for(int i = 0; i<nuevaListaCamiones.length; i++) {
+			nuevaListaCamiones[i] = listaCamiones.get(i);
+		}
+		return nuevaListaCamiones;
+		
+	}
+
+	@Override
+	public boolean addCliente(Cliente c) {
+		if(c != null && !mapaClientes.containsValue(c)) {
+			mapaClientes.put(c.getDni(), c);
+			return true;
+		}
+		else return false;
+	}
+
+	@Override
+	public boolean removeCliente(Cliente c) {
+		if(c != null && mapaClientes.containsValue(c)) {
+			mapaClientes.remove(c.getDni(), c);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Cliente buscarCliente(String dni) {
+		if(dni!= null) {
+			return mapaClientes.get(dni); 
+		}
+		else return null;
+	}
+
 }
+
